524dc592a34e2c5700790090bc58baf8ad0607f2
sorin <s.tiberiu46@yandex.com>
2017-09-05 22:08:52 +0300

    Updated API config for server

--- public/resources/views/js/app.js
+++ public/resources/views/js/app.js
@@ -4,7 +4,7 @@ angular.module('ngConfig', [])
 	.constant('config', {
 		  //api : 'http://localhost/east_backend/public/' ,
       //api : 'http://test.eastlondonestateagents.com/api/public/',
-        api : 'http://localhost:8000/api/',
+        api : 'http://13.58.142.173:8080/api/',
 	    views: '/resources/views/pages/',
       url: '/east_frontend/#!/',
 	});
